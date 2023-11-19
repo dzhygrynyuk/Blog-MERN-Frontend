287eca388afae5a87fc9fffee4e0c599c8a342ab
dzhygrynyuk <dzhygrynyuk@gmail.com>
2023-11-19 16:17:00 +0200

    Create State, PostsSlices | Get Posts

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Container from "@mui/material/Container";
 import { Routes, Route } from "react-router-dom";
 
--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
